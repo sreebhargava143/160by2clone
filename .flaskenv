bcc5e20b0d899f37226594ada0724f217ee8ee19
sreebhargava143 <sreebhargava143@gmail.com>
2019-09-30 10:35:27 +0530

    ready for deployment

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=clone160by2.py
--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,6 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -9,18 +12,12 @@ from flask import request
 from flask_moment import Moment
 from elasticsearch import Elasticsearch
 
-
-
 db = SQLAlchemy()
 migrate = Migrate()
 encrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 moment = Moment()
-
-
-
-
 
 
 def create_app(config_class=Config):
@@ -38,5 +35,22 @@ def create_app(config_class=Config):
     app.register_blueprint(users)
     app.register_blueprint(messages)
     app.register_blueprint(contacts)
+    if not app.debug and not app.testing:
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/clone160by2.log', maxBytes=10240,backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('clone160by2 startup')
 
     return app
